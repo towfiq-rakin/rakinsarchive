d1f0d598d1bb3371a8d3d3c870370d694ee7b211
Rakin <towfiqomarrakin@gmail.com>
2025-12-11 22:58:51 +0600

    Quartz sync: Dec 11, 2025, 10:58 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -89,15 +89,10 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
+      Plugin.CNAME(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      //Plugin.CustomOgImages(),
 
-      CustomOgImages({
-        colorScheme: "lightMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
-        width: 1200, // width to generate with (in pixels)
-        height: 630, // height to generate with (in pixels)
-        excludeRoot: false, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
-      }),
     ],
   },
   
