6d7933a4aef5b1ef5b44498638ab3a0f9a748f73
Rakin <towfiqomarrakin@gmail.com>
2025-12-11 23:32:40 +0600

    Quartz sync: Dec 11, 2025, 11:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -90,7 +90,7 @@ const config: QuartzConfig = {
       Plugin.NotFoundPage(),
       Plugin.CNAME(),
       // Comment out CustomOgImages to speed up build time
-      //Plugin.CustomOgImages(),
+      Plugin.CustomOgImages(),
 
     ],
   },
